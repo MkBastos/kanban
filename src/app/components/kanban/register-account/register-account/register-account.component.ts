1de262d5e51c84dbc86c08c12efe18e6f49bab52
mkbastos <miqueiaslbastos@gmail.com>
2023-04-27 04:10:16 -0300

    adicionado validadores na tela de adicionar usuário, e corrigido alguns erros na hora de gerar a tempo gasto nas tarefas finalizadas

--- src/app/components/kanban/register-account/register-account/register-account.component.ts
+++ src/app/components/kanban/register-account/register-account/register-account.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { TaskManagerService } from 'src/app/shared/services/task.manager.service';
+import { userExistsValidator } from '../../../../shared/validators/user-validator'
+import { validatePassword } from 'src/app/shared/validators/validated-password';
 
 @Component({
   selector: 'app-register-account',
@@ -12,6 +14,9 @@ export class RegisterAccountComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private service: TaskManagerService) { }
 
   addUserForm! : FormGroup;
+  password!: any;
+  userValidator = userExistsValidator;
+  passValidator = validatePassword;
 
   ngOnInit(): void {
     this.createForm()
@@ -21,13 +26,23 @@ export class RegisterAccountComponent implements OnInit {
     this.addUserForm = this.formBuilder.group({
       name: new FormControl('', [Validators.required]),
       birth_date: new FormControl('', [Validators.required]),
-      email: new FormControl(''),
-      user: new FormControl(''),
-      password: new FormControl('', [Validators.required])
+      email: new FormControl('', [Validators.email]),
+      user: ['', {Validators: [Validators.required], asyncValidators: [this.userValidator(this.service)]}],
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required, this.passValidator()])
     })
+  }
+
+  getPassword() {
+    if (this.addUserForm.get('password')?.value) {
+      this.password = this.addUserForm.get('password')?.valueChanges.subscribe(
+        next => console.log(next)
+      )
+    }
   }
 
   submit() {
     console.log(this.addUserForm.value)
+    this.service.addNewUser(this.addUserForm.value).subscribe()
   }
 }
